c078ae759c2d9937fffbf7d30acff7635d683b17
Devansh <devanshkhullar11@gmail.com>
2023-04-28 18:38:47 +0530

    fixes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,31 @@
-import React,{useState} from "react";
-import Form from '../components/LoginForm';
+import React, { useState } from "react";
+import Form from "../components/LoginForm";
 import Loading from "../components/Loading";
 
 function Login() {
-
-  const [loading,setLoading]=useState(false);
+  const [loading, setLoading] = useState(false);
 
   return (
-   <>
-   <div className="flex justify-center items-center h-screen bg-[url('/public/bgnew.svg')] " >
-    <div className="bg-white md:flex justify-between md:p-10 p-6 md:h-2/3 md:min-h-0 md:w-2/3 w-[80%] rounded-xl shadow-lg " >
-    {loading?(<div className="flex items-center justify-center mx-auto md:mt-0 mt-[80%]">
-            <Loading />
-          </div>):([<div className='flex justify-center md:w-1/2 mb-8 md:mb-0 ' >
-        <img src="/signup.gif" alt="login" />
-      </div>,
-      <div className="md:w-1/2" >
-        <Form loading={loading} setLoading={setLoading} />
-      </div>]) }
-    </div>
-   {/* <div className="text-white w-1/4 p-10 h-2/3 border-2 border-white rounded-xl bg-white" > */}
-    {/* <Form/> */}
-   {/* </div> */}
-   </div>
-   </>
+    <>
+      <div className="flex justify-center items-center h-screen bg-[url('/public/bgnew.svg')] ">
+        <div className="bg-white md:flex md:justify-between md:p-10 p-6 md:h-2/3 md:min-h-0 md:w-2/3 w-[80%] min-h-fit max-h-[80%]  rounded-xl shadow-lg ">
+          {loading ? (
+            <div className=" flex justify-center items-center mx-auto md:mt-0">
+              <Loading />
+            </div>
+          ) : (
+            [
+              <div className="flex justify-center md:w-1/2 mb-8 md:mb-0 h-48 md:h-full">
+                <img src="/signup.gif" alt="login" />
+              </div>,
+              <div className="md:w-1/2">
+                <Form loading={loading} setLoading={setLoading} />
+              </div>,
+            ]
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,24 +1,28 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Loading from "../components/Loading";
 import Form from "../components/SignupForm";
 
 function Signup() {
-
-  const [loading,setLoading]=useState(false);
+  const [loading, setLoading] = useState(false);
 
   return (
     <>
       <div className="flex justify-center items-center h-screen bg-[url('/public/bgnew.svg')] ">
-        <div className="bg-white md:flex justify-between md:p-10 p-6 md:h-2/3 md:min-h-0 md:w-2/3 w-[80%] rounded-xl shadow-lg ">
-          {loading?(<div className="flex items-center justify-center mx-auto md:mt-0 mt-[80%]">
-            <Loading />
-          </div>):([<div className='flex justify-center md:w-1/2 mb-8 md:mb-0 ' >
-        <img src="/signup.gif" alt="login" />
-      </div>,
-      <div className="md:w-1/2" >
-        <Form loading={loading} setLoading={setLoading} />
-      </div>]) }
-          
+        <div className="bg-white md:flex md:justify-between md:p-10 p-6 md:h-2/3 md:min-h-0 md:w-2/3 w-[80%] min-h-fit max-h-[81%] overflow-y-scroll rounded-xl shadow-lg ">
+          {loading ? (
+            <div className="flex justify-center items-center mx-auto md:mt-0">
+              <Loading />
+            </div>
+          ) : (
+            [
+              <div className="flex justify-center md:w-1/2 mb-8 md:mb-0 h-48 md:h-full ">
+                <img src="/signup.gif" alt="login" />
+              </div>,
+              <div className="md:w-1/2">
+                <Form loading={loading} setLoading={setLoading} />
+              </div>,
+            ]
+          )}
         </div>
       </div>
     </>
