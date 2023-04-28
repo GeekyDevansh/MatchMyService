3aee850d7dc94e5bdf15dbd882dbcc2e16445500
Devansh <devanshkhullar11@gmail.com>
2023-04-28 17:50:29 +0530

    fixes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,8 +9,8 @@ function Login() {
   return (
    <>
    <div className="flex justify-center items-center h-screen bg-[url('/public/bgnew.svg')] " >
-    <div className="bg-white md:flex justify-between md:p-10 p-6 md:h-2/3 min-h-[90%] md:min-h-0 md:w-2/3 w-[85%] rounded-xl shadow-lg " >
-    {loading?(<div className="flex items-center justify-center mx-auto">
+    <div className="bg-white md:flex justify-between md:p-10 p-6 md:h-2/3 min-h-[90%] md:min-h-0 md:w-2/3 w-[80%] rounded-xl shadow-lg " >
+    {loading?(<div className="flex items-center justify-center mx-auto md:mt-0 mt-[80%]">
             <Loading />
           </div>):([<div className='flex justify-center md:w-1/2 mb-8 md:mb-0 ' >
         <img src="/signup.gif" alt="login" />
