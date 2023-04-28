a853139abc2a64a78a692504c4a371036ac0ecbe
Devansh <devanshkhullar11@gmail.com>
2023-04-28 19:18:18 +0530

    fixes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,9 +8,9 @@ function Login() {
   return (
     <>
       <div className="flex justify-center md:items-center h-screen bg-[url('/public/bgnew.svg')] ">
-        <div className="bg-white md:flex md:justify-between md:p-10 p-6 md:h-2/3 md:min-h-0 mt-[15%] md:mt-0 md:w-2/3 w-[80%] min-h-fit max-h-[80%]  rounded-xl shadow-lg ">
+        <div className="bg-white md:flex md:justify-between md:p-10 p-6 md:h-2/3 md:min-h-0 mt-[10%] md:mt-0 md:w-2/3 w-[80%] min-h-fit max-h-[80%]  rounded-xl shadow-lg ">
           {loading ? (
-            <div className=" flex justify-center items-center mx-auto md:mt-0">
+            <div className=" flex justify-center items-center mx-auto">
               <Loading />
             </div>
           ) : (
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -8,9 +8,9 @@ function Signup() {
   return (
     <>
       <div className="flex justify-center md:items-center h-screen bg-[url('/public/bgnew.svg')] ">
-        <div className="bg-white md:flex md:justify-between md:p-10 p-6 md:h-2/3 mt-[15%] md:mt-0 md:min-h-0 md:w-2/3 w-[80%] min-h-fit max-h-[83%] overflow-y-scroll rounded-xl shadow-lg ">
+        <div className="bg-white md:flex md:justify-between md:p-10 p-6 md:h-2/3 mt-[10%] md:mt-0 md:min-h-0 md:w-2/3 w-[80%] min-h-fit max-h-[83%] overflow-y-scroll rounded-xl shadow-lg ">
           {loading ? (
-            <div className="flex justify-center items-center mx-auto md:mt-0">
+            <div className="flex justify-center items-center mx-auto">
               <Loading />
             </div>
           ) : (
