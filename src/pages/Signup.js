7ab15a93537cf8f6dbefcef008f1fe57463e82e2
Devansh <devanshkhullar11@gmail.com>
2023-04-28 18:58:19 +0530

    fixes

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -7,8 +7,8 @@ function Signup() {
 
   return (
     <>
-      <div className="flex justify-center items-center h-screen bg-[url('/public/bgnew.svg')] ">
-        <div className="bg-white md:flex md:justify-between md:p-10 p-6 md:h-2/3 md:min-h-0 md:w-2/3 w-[80%] min-h-fit max-h-[81%] overflow-y-scroll rounded-xl shadow-lg ">
+      <div className="flex justify-center md:items-center h-screen bg-[url('/public/bgnew.svg')] ">
+        <div className="bg-white md:flex md:justify-between md:p-10 p-6 md:h-2/3 mt-[15%] md:mt-0 md:min-h-0 md:w-2/3 w-[80%] min-h-fit max-h-[83%] overflow-y-scroll rounded-xl shadow-lg ">
           {loading ? (
             <div className="flex justify-center items-center mx-auto md:mt-0">
               <Loading />
